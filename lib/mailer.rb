c46fd42a73b566fd501337509f57c9f96305ce43
meanjollies <meanjollies@users.noreply.github.com>
2018-07-15 10:52:23 -0400

    No need for message to be assigned twice

--- lib/mailer.rb
+++ lib/mailer.rb
@@ -6,12 +6,12 @@ module Mailer
     if days < 1
       logger.warn "#{uid}: Password has expired. Sending notification..."
       template = File.read(File.dirname(File.expand_path(__FILE__)) + '/../conf/expired.erb')
-      message = ERB.new(template).result(binding)
     else
       logger.warn "#{uid}: Password will expire in #{days} days. Sending notification..."
       template = File.read(File.dirname(File.expand_path(__FILE__)) + '/../conf/warn.erb')
-      message = ERB.new(template).result(binding)
     end
+
+    message = ERB.new(template).result(binding)
 
     Net::SMTP.start('localhost') do |smtp|
      smtp.send_message message, sender, rcpt
